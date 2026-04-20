8661272a3112c292baf1e3e61f616c0e904b3051
kit <kit@processingfoundation.org>
2026-04-21 01:44:17 +0200

    Pondering the Orb

--- src/snippets/sphere.js
+++ src/snippets/sphere.js
@@ -1,25 +1,27 @@
 let material;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  describe("A textured sphere in the top right corner with colorful lights that follow the mouse.");
+  describe("A textured sphere in the top right corner with colorful lights that react to the mouse.");
+  noStroke();
   material = buildMaterialShader(noiseTexture);
 }
 function noiseTexture() {
   pixelInputs.begin();
-  pixelInputs.color.rgb *= noise(pixelInputs.texCoord * 5);
+  pixelInputs.color.rgb *= noise(pixelInputs.texCoord * 6 + millis()/5000);
   pixelInputs.end();
 }
 function draw() {
   shader(material);
   background(20, 0, 50);
+  ambientLight(20, 0, 20);
   
   let mx = mouseX - width / 2;
   let my = mouseY - height / 2;
 
-  pointLight(150, 130, 0, mx - 50, my, 250);
-  pointLight(0, 0, 150, mx, my, 400);
+  pointLight(150, 130, 0, mx - 50, my+500, 250);
   pointLight(200, 0, 105, mx - 80, my, 400);
+  directionalLight(0, 0, 150, width-mx/width, height-my/height, 0.5);
+  
   translate(width/3, -height/4);
-
   sphere(200);
 }
