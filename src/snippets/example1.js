5fc3b45a74169509f1e8c1f3155c4283c763350d
kit <1304340+ksen0@users.noreply.github.com>
2026-04-23 08:44:21 +0200

    Delete src/snippets/example1.js

--- src/snippets/example1.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(400, 400);
-  background(255);
-  describe("A white circle with black outline that tracks the mouse cursor as it moves.");
-}
-
-function draw() {
-  circle(mouseX, mouseY, 200);
-}
